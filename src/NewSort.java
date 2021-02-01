13e06da9c4c24bc97de0f32e609211f8291123c1
Pranjali200 <pranjalijain510@gmail.com>
2021-02-01 10:16:09 +0530

    testnewsort

--- src/NewSort.java
+++ src/NewSort.java
@@ -4,7 +4,8 @@ public class NewSort {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
     System.out.println("hii");
-    System.out.println("bye");
+    System.out.println("byee");
+    
 	}
 
 }
